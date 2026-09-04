36b5883c695fc005bbbecdf8c2fc0907accb9496
tivvo <tiveshp147@gmail.com>
2026-09-04 18:19:46 +0200

    i hate it in here

--- src/main/java/dev/celestial/silly/lua/compat/EMFCompatAPI.java
+++ src/main/java/dev/celestial/silly/lua/compat/EMFCompatAPI.java
@@ -1,14 +1,15 @@
 package dev.celestial.silly.lua.compat;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
 import org.figuramc.figura.lua.FiguraLuaRuntime;
+import org.figuramc.figura.lua.LuaNotNil;
 import org.figuramc.figura.lua.LuaWhitelist;
 
+import org.luaj.vm2.LuaTable;
 import traben.entity_model_features.EMFAnimationApi;
-
-import net.minecraft.world.entity.player.Player;
+import traben.entity_model_features.models.IEMFModel;
+import traben.entity_model_features.models.animation.EMFAnimationEntityContext;
 import traben.entity_model_features.models.parts.EMFModelPart;
-import traben.entity_model_features.models.parts.EMFModelPartCustom;
 import traben.entity_model_features.utils.EMFEntity;
 
 @LuaWhitelist
@@ -17,21 +18,26 @@ public class EMFCompatAPI extends BaseCompatAPI {
         super(runtime);
     }
 
-    // emf to require anything requires the original entity
-    // in terms of how the entity is stored in emf for the player
-    // it's like usual
-    Player clientPlayer = Minecraft.getInstance().player;
-    EMFEntity emfPlayer = EMFAnimationApi.emfEntityOf(clientPlayer);
-
     @LuaWhitelist
     public int getApiVersion() {
-
         return (EMFAnimationApi.getApiVersion());
     }
 
+    // okay, i think the best way to approach this is to rather
+    // get the model off of the emfmodel and get the parts from there so we're gonna test a bit
+    // tbh
     @LuaWhitelist
-    public double getEntityXPos() {
-        return (emfPlayer.emf$getX());
+    public boolean doesEMFEntityExist(@LuaNotNil Entity checkEnt) {
+        return (EMFAnimationApi.emfEntityOf(checkEnt) != null);
     }
 
+    @LuaWhitelist
+    public EMFEntity getCurrentEMFEntity() {
+        return EMFAnimationApi.getCurrentEntity();
+    }
+
+    @LuaWhitelist
+    public void test() {
+
+    }
 }
